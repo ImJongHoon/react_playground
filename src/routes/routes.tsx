e37e34d51550d758853ee40ef8e12bc3a5dad211
imjh990614 <imjh990614@gmail.com>
2025-01-09 17:55:32 +0900

    Router testing

---
+++ src/routes/routes.tsx
@@ -0,0 +1,9 @@
+import { BrowserRouter } from "react-router-dom";
+import {Headers} from "./components/Headers"
+function Router(){
+    return <BrowserRouter>
+        <Headers></Headers>
+    </BrowserRouter>
+}
+
+export Router;
